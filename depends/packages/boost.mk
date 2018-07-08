9c89efdbda5d1f1b1a4be5ad2b546fda1c2dcd2e
AegeusCoin <webmaster@aegeus.io>
2018-07-08 06:29:36 +0000

    boost 1.54 test linux

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,8 +1,8 @@
 package=boost
-$(package)_version=1_63_0
-$(package)_download_path=https://sourceforge.net/projects/boost/files/boost/1.63.0
+$(package)_version=1_54_0
+$(package)_download_path=https://sourceforge.net/projects/boost/files/boost/1.54.0
 $(package)_file_name=$(package)_$($(package)_version).tar.bz2
-$(package)_sha256_hash=beae2529f759f6b3bf3f4969a19c2e9d6f0c503edcb2de4a61d1428519fcb3b0
+$(package)_sha256_hash=047e927de336af106a24bceba30069980c191529fd76b8dff8eb9a328b48ae1d
 
 define $(package)_set_vars
 $(package)_config_opts_release=variant=release
